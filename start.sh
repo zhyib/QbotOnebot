4cb8d26f92bb3953e08b706bf5121799d38a6d72
zhyib <zhyib002@gmail.com>
2020-12-02 21:34:46 +0800

    update: start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
+echo 'Cleaning cache...'
 rm data/image/*
+echo 'Done'
+
+echo 'Git pull...'
 git pull
+echo 'Done'
+
+echo 'Updating dependencies...'
 npm i
+echo 'Done'
+
+echo 'Start build...'
 npm run build
