c0ced34f28e8b933c6665d4d724380a1dc6f9b1b
zhyib <zhyib002@gmail.com>
2021-03-17 18:21:43 +0800

    feature: offline

--- components/messageHandler.js
+++ components/messageHandler.js
@@ -1,14 +1,36 @@
 const command = require('./command');
 const listener = require('./listener');
 
+let online = true;
+
 function messageHandler(bot, data) {
   // console.log(data);
   try {
     const message = data.raw_message;
-    if (message[0] === '#') {
-      command(bot, data);
-    } else {
-      listener(bot, data);
+    if (message === '#下线') {
+      if (online) {
+        online = false;
+        bot.sendGroupMsg(data.group_id, 'bot已下线，将在10min后上线');
+        setTimeout(() => {
+          if (!online) {
+            online = true;
+            bot.sendGroupMsg(data.group_id, 'bot已上线');
+          }
+        }, 600000);
+      }
+    }
+    if (message === '#上线') {
+      if (!online) {
+        online = true;
+        bot.sendGroupMsg(data.group_id, 'bot已上线');
+      }
+    }
+    if (online) {
+      if (message[0] === '#') {
+        command(bot, data);
+      } else {
+        listener(bot, data);
+      }
     }
   } catch (error) {
     console.error(error);
