2ea07c4de193949f082447bf0e3564e16a13ee08
zhyib <zhyib002@sina.com>
2021-05-12 15:42:41 +0800

    remove: paint

--- components/commands/replyRect.js
+++
@@ -1,8 +0,0 @@
-const paint = require('@utils/paint');
-
-module.exports = function replyRect(bot, data) {
-  const message = data.raw_message;
-  const [cmd, color] = message.split(' ');
-  paint.paintRect(color);
-  bot.sendGroupMsg(data.group_id, '[CQ:image,file=./components/utils/image.png]');
-};
