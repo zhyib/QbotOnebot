7ff3dd7468626f73e98a5f85a00c41cb6566317f
zhyib <zhyib002@outlook.com>
2021-05-17 16:14:58 +0800

    fix: mutli group bugs

--- src/components/module/moduleTea.js
+++ src/components/module/moduleTea.js
@@ -1,6 +1,7 @@
 module.exports = function moduleTea(bot, dataIn, now) {
   console.log(now);
-  const groupId = dataIn?.group_id || [166795834, 782842016];
+  const subGroups = [166795834, 782842016];
+  const groupId = dataIn ? [dataIn.groupId] : subGroups;
   groupId.forEach((element) => {
     bot.sendGroupMsg(element, '[CQ:image,file=./data/local/tea.jpg]');
   });
